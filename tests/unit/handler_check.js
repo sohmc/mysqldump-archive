4c939d1c2fcf6da3731af32fdedf1e8c9e3aaa3a
Michael Soh <sohmc@users.noreply.github.com>
2018-10-25 15:11:40 -0400

    Added first handler check

---
+++ tests/unit/handler_check.js
@@ -0,0 +1,10 @@
+const LambdaTester = require( 'lambda-tester' );
+const myHandler = require( '../../index.js' ).handler;
+ 
+describe( 'handler', function() {
+    it( 'test success', function() {
+        return LambdaTester( myHandler )
+            .event( { name: 'Fred' } )
+            .expectResult();
+    });
+});
