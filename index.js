2e9f9fede874871723cb4ea0a9ea68bd606005d8
Michael Soh <sohmc@users.noreply.github.com>
2018-10-25 16:24:36 -0400

    Fixed bucket name and now moving to deleting old latest files

--- index.js
+++ index.js
@@ -9,38 +9,33 @@ exports.handler = async (event, context) => {
     // Get the object from the event and show its content type
     // const bucket = event.Records[0].s3.bucket.name;
     // const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    const params = {
-        Bucket: "mikesoh.com-galactica-backup",
-        Prefix: "mysql-backups",
-        StartAfter: "mysql-backups"
-    };
     
     var event_record = event.Records[0];
 
-    console.log("Bucket: ", event_record.s3.bucket);
+    console.log("Bucket: ", event_record.s3.bucket.name);
     console.log("Event Object: ", event_record.s3.object.key)
-   
+  
+    console.log("COPY OBJECT: ", "Got the file and about to copy it into latest...");
     const copyParams = {
-        CopySource: "/" + event_record.s3.bucket + "/" + event_record.key,
-        Bucket: event_record.s3.bucket,
+        CopySource: "/" + event_record.s3.bucket.name + "/" + event_record.key,
+        Bucket: event_record.s3.bucket.name,
         Key: event_record.s3.object.key.replace('mysql-backups','mysql-backups/atest'),
         ServerSideEncryption: "AES256"
     };
 
     console.log('copyParams', copyParams);
-    foo("Done");
-/*  
+
     await s3.copyObject(copyParams, function(copyErr, copyData) {
         if (copyErr) console.log(copyErr, copyErr.stack);
         else console.log(copyData);
     }).promise();
 
-    foo("Done");
+    const params = {
+        Bucket: copyParams.Bucket,
+        Prefix: "mysql-backups/latest",
+        StartAfter: "mysql-backups/latest/"
+    };
 
-    // Use current date and time 
-    var oldest_datetime = new Date();
-    var oldest_object = null;
-   
     await s3.listObjectsV2(params, function(err, data) {
         if (err) {
             console.log("error", "There was an error")
@@ -49,25 +44,25 @@ exports.handler = async (event, context) => {
             console.log(data)
             console.log("Key Count: ", data.KeyCount)
 
-            // If there is more than one file, then loop through.
-            if (data.KeyCount > 1) {
-                for (var i = 0; i < data.KeyCount; i++) {
-                    var obj = data.Contents[i];
-                    
-                    console.log(i, ": " + obj.Key)
-                    console.log(i, ": " + obj.LastModified)
-                    
-                    if (oldest_datetime >= obj.LastModified) {
-                        console.log(i, ": found older object");
-                        oldest_datetime = obj.LastModified;
-                        oldest_object = obj
-                    }
+            for (var i = 0; i < data.KeyCount; i++) {
+                var obj = data.Contents[i];
+                
+                console.log(i, ": " + obj.Key)
+                console.log(i, ": " + obj.LastModified)
+
+                if (obj.Key.replace(params.Prefix, 'mysql-backups') != copyParams.Key) {
+                    console.log("DELETE: ", "Will delete " + obj.Key)
                 }
-            } else {
-                console.log("No oldest object but the event object.")
             }
         }
     }).promise();
+
+    foo("Done");
+/*  
+    // Use current date and time 
+    var oldest_datetime = new Date();
+    var oldest_object = null;
+   
 
     if (oldest_object) {    
         const copyParams = {
